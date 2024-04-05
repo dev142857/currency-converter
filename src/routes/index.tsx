14fe0a760476a2ab072b1b2f34c4d36d556224a3
dev142857 <genuine5621@gmail.com>
2024-04-05 06:08:10 -0700

    change port

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { Suspense } from 'react'
+import { BrowserRouter, Router, Route, Routes } from 'react-router-dom'
 
 const AppRoutes = () => {
-  return (
-    <div>AppRoutes</div>
-  )
+    return (
+        <BrowserRouter>
+            <>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Routes>
+                        <Route path='/' element='asdf' />
+                    </Routes>
+                </Suspense>
+            </>
+        </BrowserRouter>
+    )
 }
 
 export default AppRoutes
---
+++ src/routes/route.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const PublicRoutes = [
+    {
+        slug: '/',
+        component: '',
+    }
+]
+
+// export default PublicRoutes
